030a01d1db2dc3390a750c41a4bf1528295ce16b
JoyEmma12 <ejoy0600@gmail.com>
2022-10-24 18:30:39 +0100

    task 0

---
+++ low-level_program/array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints array 1
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int myNumber[] = {25, 50, 75, 100};
+
+printf("%d\n", myNumber[1]);
+
+return (0);
+}
